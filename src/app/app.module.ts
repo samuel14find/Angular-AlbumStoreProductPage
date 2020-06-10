5a214c1366a3a867e41daf075d7c0fe034ba1357
samuel14find <bicalho.sam@gmail.com>
2020-06-10 11:02:48 -0300

    Criação do componente product-list. Criei a interface product. Fiz o getProducts no ProductService para pegar a lista no json products.json. Teste no app.component.html para ver se funcionou

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,8 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
-import {ProductTracklistingComponent} from './product-tracklisting/product-tracklisting.component'
+import {ProductTracklistingComponent} from './product-tracklisting/product-tracklisting.component';
+import { ProductListComponent } from './product-list/product-list.component'
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import {ProductTracklistingComponent} from './product-tracklisting/product-track
     AppComponent,
     ProductPageComponent,
     ProductDescriptionComponent,
-    ProductTracklistingComponent
+    ProductTracklistingComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
